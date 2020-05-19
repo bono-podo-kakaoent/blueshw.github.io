bc0bfd129d91f8f9a0d1ab32e9b431c7372dbbee
bono <bono@kakaopage.com>
2020-05-19 23:17:12 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c6065a5e066217767ff0.js"
+    "url": "webpack-runtime-d6a1fd64220b59730593.js"
   },
   {
     "url": "framework-c034a59c69769c9f4a47.js"
   },
   {
-    "url": "app-11b3c027aedda7ebd9e8.js"
+    "url": "app-ea10543fd8495771beb1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "33573c9a23ff6d68454d4133a74c0972"
+    "revision": "594fda566b7d4d141cd12aea6b611de6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-99ed5caf1e3f27d98142.js"
@@ -137,7 +137,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-11b3c027aedda7ebd9e8.js`))) {
+  if (!resources || !(await caches.match(`/app-ea10543fd8495771beb1.js`))) {
     return await fetch(event.request)
   }
 
